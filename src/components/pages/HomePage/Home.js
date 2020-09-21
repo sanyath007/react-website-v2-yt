a7cb7d6929db5d92d9993d93e8fe378c5a1699e0
Sanyath Thammawong <sanyath007@gmail.com>
2020-09-21 13:59:40 +0700

    Build Pricing component and add its to Home

--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -6,6 +6,7 @@ import {
   homeObjThree,
   homeObjFour
 } from './Data';
+import Pricing from '../../Pricing/Pricing';
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
       <HeroSection {...homeObjOne} />
       <HeroSection {...homeObjThree} />
       <HeroSection {...homeObjTwo} />
+      <Pricing />
       <HeroSection {...homeObjFour} />
     </>
   )
